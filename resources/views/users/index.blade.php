3172d1ad40aed96c9edebbb9ef5ca8ace80d6bd4
surajkashyap <surajkashyapp9911@gmail.com>
2023-12-23 16:36:19 +0530

    Create index page of users.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,11 +8,11 @@
 
         <!-- PAGE-HEADER -->
         <div class="page-header">
-            <h1 class="page-title">Form-Layouts</h1>
+            <h1 class="page-title">Users</h1>
             <div>
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="javascript:void(0)">Forms</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Form-Layouts</li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">Users</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Index</li>
                 </ol>
             </div>
         </div>
@@ -20,76 +20,65 @@
 
         <!-- Row -->
         <div class="row">
-            <div class="col-md-12 col-xl-6">
+            <div class="col-lg-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Vertical Form</h4>
+                    <div class="card-header d-flex justify-content-between">
+                        <h4 class="card-title">
+                            Users
+                        </h4>
+
+                        <a href="{{ route('users.create') }}" class="btn btn-primary float-right">Create User</a>
                     </div>
+
                     <div class="card-body">
-                        <form>
-                            <div class="">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1"
-                                        placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        placeholder="Password">
-                                </div>
-                                <label class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="example-checkbox2"
-                                        value="option2">
-                                    <span class="custom-control-label">Check me Out</span>
-                                </label>
-                            </div>
-                            <button class="btn btn-primary mt-4 mb-0">Submit</button>
-                        </form>
+                        <div class="table-responsive">
+                            <table class="table text-nowrap text-md-nowrap mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Position</th>
+                                        <th>Salary</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Joan Powell</td>
+                                        <td>Associate Developer</td>
+                                        <td>$450,870</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Gavin Gibson</td>
+                                        <td>Account manager</td>
+                                        <td>$230,540</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Julian Kerr</td>
+                                        <td>Senior Javascript Developer</td>
+                                        <td>$55,300</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Cedric Kelly</td>
+                                        <td>Accountant</td>
+                                        <td>$234,100</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>Samantha May</td>
+                                        <td>Junior Technical Author</td>
+                                        <td>$43,198</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-12 col-xl-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Horizontal Form</h4>
-                    </div>
-                    <div class="card-body">
-                        <form class="form-horizontal">
-                            <div class=" row mb-4">
-                                <label for="inputName" class="col-md-3 form-label">User Name</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" id="inputName" placeholder="Name">
-                                </div>
-                            </div>
-                            <div class=" row mb-4">
-                                <label for="inputEmail3" class="col-md-3 form-label">Email</label>
-                                <div class="col-md-9">
-                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class=" row mb-4">
-                                <label for="inputPassword3" class="col-md-3 form-label">Password</label>
-                                <div class="col-md-9">
-                                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                                </div>
-                            </div>
-                            <div class="mb-0 row justify-content-end">
-                                <div class="col-md-9">
-                                    <label class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" name="example-checkbox2"
-                                            value="option2">
-                                        <span class="custom-control-label">Check me Out</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="mb-0 mt-4 row justify-content-end">
-                                <div class="col-md-9">
-                                    <button class="btn btn-primary">Sign in</button>
-                                    <button class="btn btn-secondary">Cancel</button>
-                                </div>
-                            </div>
-                        </form>
+
+                    <div class="card-footer">
+
                     </div>
                 </div>
             </div>
