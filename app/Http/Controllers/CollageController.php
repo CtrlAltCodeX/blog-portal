5934a09790232adaf13502232a1c339f41a9ac83
surajkashyap <surajkashyapp9911@gmail.com>
2024-03-10 13:17:13 +0530

    collage images changes,

--- app/Http/Controllers/CollageController.php
+++ app/Http/Controllers/CollageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Generators\CustomThreeImage;
 use Illuminate\Support\Facades\Response;
 use Intervention\Image\Facades\Image;
+use Tzsk\Collage\Generators\FourImage;
+use Tzsk\Collage\Generators\OneImage;
+use Tzsk\Collage\Generators\TwoImage;
 use Tzsk\Collage\MakeCollage;
 
 class CollageController extends Controller
@@ -19,6 +23,7 @@ class CollageController extends Controller
     public function store()
     {
         $this->validate(request(), [
+            'file'   => 'required|array',
             'file.*' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
 
@@ -29,16 +34,30 @@ class CollageController extends Controller
         foreach ($files['file'] as $file) {
             $image = Image::make($file);
 
-            $image->resize(300, 200);
-
             $processedImages[] = $image;
         }
 
         $collage = new MakeCollage();
 
-        $image = $collage->make(400, 400)->padding(10)->from($processedImages);
+        $image = $collage->with([
+            1 => OneImage::class,
+            2 => TwoImage::class,
+            3 => CustomThreeImage::class,
+            4 => FourImage::class,
+        ])->make(400, 400)->padding(10)->from($processedImages, function($alignment) use($processedImages) {
+            $imageCount = count($processedImages);
+
+            if ($imageCount == 2) {
+                $alignment->vertical();
+            } else if ($imageCount == 3) {
+                $alignment->twoTopOneBottom();
+            } else if ($imageCount == 4) {
+                $alignment->grid();
+            }
+        });
 
         $filename = 'collage_' . time() . '.png';
+
         $image->save(public_path('storage/' . $filename));
 
         $imageContent = file_get_contents(public_path('storage/' . $filename));
