f95ab1537f9c467151066bd8507fae3337b2094f
Lalan <kumarlalan38@gmail.com>
2024-12-12 19:36:52 +0530

    chatgpt secure changes

--- app/Http/Controllers/ChatGptController.php
+++ app/Http/Controllers/ChatGptController.php
@@ -20,7 +20,7 @@ class ChatGptController extends Controller
         $data = Http::withHeaders([
                     'Content-Type' => 'application/json',
                     'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
-                ])->post("https://api.openai.com/v1/chat/completions", [
+                ])->post(env('OPENAI_API_URL'), [
                     "model" => "gpt-3.5-turbo",
                     'messages' => [
                        [
